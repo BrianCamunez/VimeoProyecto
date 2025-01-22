ad0be9a919a7cc9dad7bdb25894093cdfc182e07
BrianCamunez <camunezbenitezbrian@fpllefia.com>
2025-01-22 20:47:06 +0100

    arreglo carpeta dist

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite';
 import path from 'path';
 
 export default defineConfig({
- root: path.resolve(__dirname, 'src'),
- publicDir: path.resolve(__dirname, 'public'),
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-        },
+  // Asegurándote de que 'root' apunte a la carpeta 'src'
+  root: path.resolve(__dirname, 'src'),
+
+  publicDir: path.resolve(__dirname, 'public'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
+  },
+  build: {
+    // 'dist' se creará en la raíz del proyecto
+    outDir: path.resolve(__dirname, 'dist'),
+  },
 });
